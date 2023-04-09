279d1a4dc2a04d48f36955486a923f016a93d356
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-04-09 16:29:33 +0530

    updated

--- YSLcoWeb/src/app/app.component.ts
+++ YSLcoWeb/src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'YSLcoWeb';
   srchtxt: string = '';
+  mainbg = '';
+  darkmode = false;
+  outputtxt: string = 'uqeuueqb\nqbbbb\nqenenqern\nqernqnr\qnenrqnne\nqenqrenrqeneqn\nqeenqernqen';
+  changebg()
+  {
+    if(!this.darkmode)
+    {
+      this.mainbg = 'background-color : #313744';
+      this.darkmode = true;
+    }
+    else if(this.darkmode)
+    {
+      this.mainbg = 'background-color : #d8dee9';
+      this.darkmode = false;
+    }
+  }
 }
